f40754532a8fcb8fc0ba718216e4680a1f507e76
Violeta Vaideanu <violetavaideanu@yahoo.com>
2021-03-25 16:42:34 +0200

    magazin online

---
+++ proiectFinal/admin.js
@@ -0,0 +1,134 @@
+let url = "https://magazinonline-a7a77-default-rtdb.europe-west1.firebasedatabase.app/";
+let id = window.location.search.substr(4);
+let listaProduseAdmin = [];
+let produs = {};
+
+
+// Admin Page
+
+async function getListaProduseAdmin() {
+    let response = await fetch (url+".json");
+    listaProduseAdmin = await response.json();
+    drawProduseAdmin();
+}
+
+function drawProduseAdmin(){
+    let str = "";
+    let produsCautat = document.querySelector('[name="search"]').value;
+    for (let [i,produs] of Object.entries(listaProduseAdmin)){
+        if(produs===null){
+            continue;
+        }
+        if(!produs.nume.toLowerCase().includes(produsCautat)){
+            continue;
+        }
+        str+=`
+        <tr>
+            <td><img src="${produs.poza}"/></td>
+            <td><a href="edit.html?id=${i}">${produs.nume}</a></td>
+            <td>${produs.pret}.00 Lei</td>
+            <td>${produs.cantitate}</td>
+            <td class="remove-btn"><a href="delete.html?id=${i}">Remove</a></td>
+        </tr>  
+        `
+    }
+    document.querySelector(".listaAdmin").innerHTML = str;
+}
+
+
+// Adauga Page
+
+async function salveaza(event){
+    event.preventDefault();
+    let poza = document.querySelector("[name='poza']").value;
+    let nume = document.querySelector("[name='nume']").value;
+    let descriere = document.querySelector("[name='descriere']").value;
+    let pret = document.querySelector("[name='pret']").value;
+    let cantitate = document.querySelector("[name='cantitate']").value;
+    
+    let produsNou = {
+        "nume": nume,
+        "poza": poza,
+        "descriere": [descriere],
+        "pret": pret,
+        "cantitate": Number(cantitate)
+    };
+
+    let response = await fetch(url+".json", {
+        method:"post",
+        body: JSON.stringify(produsNou),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    });
+    produs = await response.json();
+    document.querySelector("form").reset();
+}
+
+
+// Delete Page
+
+async function getProdusDel() {
+    let response = await fetch (url+id+".json");
+    produs = await response.json();
+    drawNume();
+}
+
+function drawNume(){
+    document.querySelector("#nume").innerText = produs.nume;
+}
+
+async function sterge(){
+    let response = await fetch(url+id+".json", {
+        method:"delete"
+    });
+    produs = await response.json();
+    window.location = "admin.html";  
+}
+
+
+//Edit Page
+
+async function getProdusEdit() {
+    let response = await fetch (url+id+".json");
+    produs = await response.json();
+    drawProdusEdit();
+}
+
+function drawProdusEdit(){
+	document.querySelector("[name='poza']").value = produs.poza;
+    document.querySelector("[name='nume']").value = produs.nume;
+    document.querySelector("[name='pret']").value = produs.pret;
+    document.querySelector("[name='cantitate']").value = produs.cantitate;
+    document.querySelector("[name='descriere']").value = produs.descriere;
+}
+
+async function salveazaEdit(event){
+    event.preventDefault();
+    let poza = document.querySelector("[name='poza']").value;
+    let nume = document.querySelector("[name='nume']").value;
+    let descriere = document.querySelector("[name='descriere']").value;
+    let pret = document.querySelector("[name='pret']").value;
+    let cantitate = document.querySelector("[name='cantitate']").value;
+    
+    let produsNou = {
+        "nume": nume,
+        "poza": poza,
+        "descriere": [descriere],
+        "pret": pret,
+        "cantitate": Number(cantitate)
+    };
+
+    let response = await fetch(url+id+".json", {
+        method:"put",
+        body: JSON.stringify(produsNou),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+    });
+    produs = await response.json();
+
+    window.location = "admin.html";
+}
+
+
---
+++ proiectFinal/main.js
@@ -0,0 +1,247 @@
+//Products Page
+
+let url = "https://magazinonline-a7a77-default-rtdb.europe-west1.firebasedatabase.app/";
+let id = window.location.search.substr(4);
+let listaProduse = [];
+let produs = {};
+let cartStr = localStorage.getItem("cart");
+let cart = JSON.parse(cartStr);
+	
+
+
+async function getListaProduse() {
+    document.querySelector("#loading").classList.remove("hidden");
+    let response = await fetch (url+".json");
+    listaProduse = await response.json();
+    document.querySelector("#loading").classList.add("hidden");
+    drawProduse();
+    countCart();
+}
+
+function drawProduse(){
+    let str = "";
+    let produsCautat = document.querySelector('[name="search"]').value;
+    for (let [i,produs] of Object.entries(listaProduse)){
+        if(produs===null){
+            continue;
+        }
+        if(!produs.nume.toLowerCase().includes(produsCautat)){
+            continue;
+        }
+        str+=`
+            <div class="product">
+                <div class="img-container">
+                    <img src="${produs.poza}" class="product-img" alt="${produs.nume}"/>
+                    <div class="viewBtn"><a href="details.html?id=${i}">View</a></div>
+                    <h3>${produs.nume}</h3>
+                    <h4>${produs.pret}.00 Lei</h4>
+                </div>
+            </div>
+        `
+    }
+    document.querySelector(".products-container").innerHTML = str;
+}
+
+
+//Details Page
+
+async function getProdus() {
+    document.querySelector("#loading").classList.remove("hidden");
+    let response = await fetch (url+id+".json");
+    produs = await response.json();
+    document.querySelector("#loading").classList.add("hidden");
+    drawDetalii();
+    countCart();
+}
+
+function drawDetalii(){
+
+	document.querySelector("#poza").src = produs.poza;
+    document.querySelector("#nume").innerText = produs.nume;
+    document.querySelector("#pret").innerHTML = `<strong>Pret:</strong> <span>${produs.pret}.00 Lei</span>`;
+    document.querySelector("#cantitate").innerHTML = `<strong>In stoc:</strong> ${produs.cantitate} buc`;
+    document.querySelector("#descriere").innerHTML = `
+    <dt><strong>Descriere produs:</strong></dt>
+    <dd>${produs.descriere}</dd>
+    `
+}
+
+
+function addToCart() {
+    let poza = produs.poza;
+    let nume = produs.nume;
+    let quantity = document.querySelector("#articleQuantity").value;
+    quantity = Number(quantity);
+    let pret = produs.pret;
+    let stoc = produs.cantitate;
+    let cartStr = localStorage.getItem("cart");
+    let cart = [];
+	if(cartStr !== null){
+		cart = JSON.parse(cartStr);
+	}
+    let found = false;
+    for (let item of cart) {
+        if (item.id === id) {
+            found = true;
+            if(item.quantity + quantity > stoc){
+                alert("CANTITATEA SELECTATA DEPASESTE STOCUL DISPONIBIL!");
+            } else {
+                item.quantity += quantity;
+                showToast();
+                stoc -= quantity;
+            }
+            break;
+        }
+    }
+    
+    if (!found && quantity > stoc) {
+        alert("CANTITATEA SELECTATA DEPASESTE STOCUL DISPONIBIL!");
+    }
+    if (!found &&  quantity <= stoc) {
+            cart.push({ 
+            id: id, 
+            quantity: quantity,
+            nume: nume,
+            pret: pret,
+            poza: poza,
+        });
+        stoc -= quantity;
+        showToast();
+    } 
+    localStorage.setItem("cart", JSON.stringify(cart));
+    countCart();
+}
+
+function countCart() {
+    let cartStr = localStorage.getItem("cart");
+    let cart = [];
+	if(cartStr !== null){
+		cart = JSON.parse(cartStr);
+	}
+    let counter = 0;
+    for (let i=0; i<cart.length; i++){
+        counter += cart[i].quantity;
+    }
+    document.querySelector(".cart-items").innerHTML = counter;
+}
+
+
+function showToast(){
+    let toast = document.getElementById("toast");
+    toast.classList.add("show");
+    toast.innerHTML = (`Produsul "${produs.nume}" a fost adaugat in cos!`);
+    setTimeout(function(){
+        toast.classList.remove("show");
+    },3000);
+}
+
+
+// Cart Page
+
+function drawCart() {
+    document.querySelector("#loading").classList.remove("hidden");
+    let str = "";
+    for (let i=0; i<cart.length; i++){
+        str += `
+        <div class="cart-item">
+            <form>
+                <div class="cart-product">
+                <a href="details.html?id=${cart[i].id}">
+                    <div class="cart-image" style="background-image: url(${cart[i].poza})"></div>
+                    <div class="cart-product-info">
+                        <p class="cart-product-name">${cart[i].nume}</p>
+                </a>
+                        <p class="cart-price-sm">${cart[i].pret}.00 Lei</p>
+                    </div>
+                </div>
+                <div class="cart-quantity-md">
+                    <div class="cart-quantity-controls">
+                        <button onclick="decrease('${cart[i].id}');">-</button>
+                        <input type="text" class="qty" value="${cart[i].quantity}" />
+                        <button onclick="increase('${cart[i].id}');">+</button>
+                    </div>
+                </div>
+                <div class="cart-unit-price">
+                    <h4>${cart[i].pret}.00 Lei</h4>
+                </div>
+                <div class="cart-product-subtotal">
+                    <h4>${cart[i].pret * cart[i].quantity}.00 Lei</h4>
+                </div>
+                <div class="cart-remove">
+                    <input id="stergeBtn" onclick="sterge('${i}');" class="btn" type="button" value="Remove"/>
+                </div>
+                <div class="cart-controls-sm">
+                    <button class="remove" onclick="sterge('${i}');"><i class="fas fa-trash-alt"></i> Remove </button>
+                    <div class="cart-quantity-controls-sm">
+                        <button onclick="decrease('${cart[i].id}');">-</button>
+                        <input type="text" class="qty" value="${cart[i].quantity}"/>
+                        <button onclick="increase('${cart[i].id}');">+</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        `
+    }
+    document.querySelector(".cart-item").innerHTML = str;
+    totalCart();
+    countCart();
+    document.querySelector("#loading").classList.add("hidden");
+}
+
+
+function decrease(id) {
+    for (let i=0; i<cart.length; i++){
+        if (cart[i].id === id && cart[i].quantity > 1 ) {
+            cart[i].quantity -= 1;
+        }
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    drawCart();
+}
+
+function increase(id) {
+    for (let i=0; i<cart.length; i++){
+        if (cart[i].id === id){
+            cart[i].quantity += 1;
+        }
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    drawCart();
+}
+
+function sterge(i) {
+    cart.splice(i, 1);
+    localStorage.setItem("cart", JSON.stringify(cart));
+    drawCart();
+}
+
+function totalCart() {
+    let total = 0;
+    for (let i=0; i<cart.length; i++){ 
+        let itemQuantity = cart[i].quantity;
+        let itemPret = cart[i].pret;
+        total += itemQuantity * itemPret;
+        document.querySelector("#total").innerHTML = `Total: ${total}.00 Lei`;
+    }
+}
+
+function buy(){
+    Swal.fire({
+        type: "success",
+        title: "Success",
+        text: "Operation Completed"
+    });
+    setTimeout(function(){
+        window.location = "index.html";
+    },3500);
+}
+
+function goBack() {
+    window.history.back();
+  }
+
+
+
+
+
+
