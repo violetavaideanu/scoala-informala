94f683713e793fab93acd954e15810fc157f1021
Violeta Vaideanu <violetavaideanu@yahoo.com>
2021-03-26 08:03:01 +0200

    add scroll toTOP

--- proiectFinal/admin.js
+++ proiectFinal/admin.js
@@ -131,4 +131,13 @@ async function salveazaEdit(event){
     window.location = "admin.html";
 }
 
+let toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", ()=>{
+    if(window.pageYOffset >100){
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
 
--- proiectFinal/main.js
+++ proiectFinal/main.js
@@ -238,7 +238,17 @@ function buy(){
 
 function goBack() {
     window.history.back();
-  }
+}
+
+let toTop = document.querySelector(".to-top");
+window.addEventListener("scroll", ()=>{
+    if(window.pageYOffset >100){
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
 
 
 
