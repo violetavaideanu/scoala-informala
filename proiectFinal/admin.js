98482a40097d913f02e3ef5a54db8df0f036c7bb
Violeta Vaideanu <violetavaideanu@yahoo.com>
2021-07-15 17:33:16 +0300

    update database

--- proiectFinal/admin.js
+++ proiectFinal/admin.js
@@ -1,4 +1,4 @@
-let url = "https://ceramicsart-2b19c-default-rtdb.europe-west1.firebasedatabase.app/";
+let url = "https://ceramicsart-ecc29-default-rtdb.europe-west1.firebasedatabase.app/";
 let id = window.location.search.substr(4);
 let listaProduseAdmin = [];
 let produs = {};
