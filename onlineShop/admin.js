386c86f56ee3ed628cb60dd726e5359c200f3f9b
Violeta Vaideanu <74261397+violetavaideanu@users.noreply.github.com>
2022-07-25 14:08:05 +0300

    updt firebase

--- onlineShop/admin.js
+++ onlineShop/admin.js
@@ -1,4 +1,4 @@
-let url = "https://ceramicshop-54db5-default-rtdb.europe-west1.firebasedatabase.app/";
+let url = "https://magazinonline-d9935-default-rtdb.europe-west1.firebasedatabase.app/";
 let id = window.location.search.substr(4);
 let listaProduseAdmin = [];
 let produs = {};
